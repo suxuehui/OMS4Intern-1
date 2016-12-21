fe9ec58a5bd93ae4da09650e427da5d22794d9b9
马潇霄 <MAVI003@nkgw8nh1411.asycn.loc>
2016-12-21 11:00:31 +0800

    添加 登录， 用户列表 页面jsp ，js，删除 helloword index.jsp

---
+++ web-ssm/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,99 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: MAVI003
+  Date: 2016/12/15
+  Time: 11:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>登录</title>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/login.css">
+    <script src="../js/jquery-3.1.1.min.js"></script>
+    <script>
+        $(function () {
+
+            $("#loginsubmit").click(function () {
+                var userId = $("#userId");
+                var password = $("#password");
+                var msg = $("#msg");
+                if ($.trim(userId.val()) == "") {
+                    userId.focus();
+                    msg.html("<br/>用户名不能为空");
+                    return false;
+                } else if ($.trim(password.val()) == "") {
+                    password.focus();
+                    msg.html("<br/>密码不能为空");
+                    return false;
+                } else {
+                    var checkUNameUrl = "checkUName?uName=" + userId.val();
+                    $.get(checkUNameUrl, function (str) {
+                        if (str == 0) {
+                            userId.val("");
+                            password.val("");
+                            userId.focus();
+                            msg.html("<br/>用户名不存在");
+                            return false;
+                        } else {
+                            $.ajax({
+                                type: "POST",
+                                url: "checkUser",
+                                data: "uname=" + userId.val() + "&password=" + password.val(),
+                                dataType: "json",
+                                success: function (data) {
+                                    if (data.check == 0) {
+                                        alert("请输入正确密码")
+                                    } else if (data.check == 1) {
+                                        alert("管理员");
+                                        window.location.href="index?urole="+1;
+
+                                    } else if (data.check == 2) {
+                                        alert("普通用户");
+                                        window.location.href="index?urole="+2;
+                                    }else {
+                                        alert("用户身份异常，此用户失效")
+                                    }
+                                }
+                            });
+
+                        }
+                    });
+                    return false;
+                }
+            })
+        })
+
+    </script>
+</head>
+
+<body>
+<div class="wrap">
+    <div class="login">
+        <div class="logo">
+            <img src="${pageContext.request.contextPath}/images/logo.jpg" class="logoImg">
+        </div>
+        <div class="loginMain">
+            <form>
+                <div class="formList">
+                    <ul>
+                        <li>
+                            <label class="listName" for="用户名">用户名：</label>
+                            <input type="text" name="" class="inputList" id="userId">
+                        </li>
+                        <li>
+                            <label class="listName" for="密码">密码：</label>
+                            <input type="password" name="" class="inputList ml20" id="password">
+                        </li>
+                        <label class="msg" id="msg"></label>
+                        <li><input type="submit" value="" class="submitBtn" align="center" id="loginsubmit"></li>
+                    </ul>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
---
+++ web-ssm/src/main/webapp/js/userlistAndGoodslist.js
@@ -0,0 +1,328 @@
+$.extend({
+    getUrlVars: function () {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    },
+    getUrlVar: function (name) {
+        return $.getUrlVars()[name];
+    }
+});
+
+//获取参数对象
+//alert($.getUrlVars());
+//获取参数a的值
+//alert($.getUrlVar('urole'));
+
+
+function usercheckclick(userid) {
+
+    var count = 0;
+    var checkArry = document.getElementsByName("usercheck");
+    for (var i = 0; i < checkArry.length; i++) {
+        if (checkArry[i].checked == true) {
+            ++count;
+            if (count > 1) {
+                document.getElementById("updateUserBut").disabled = true;
+
+                return;
+            }
+            document.getElementById("updateUserBut").disabled = false;
+
+        }
+    }
+}
+
+$(document).ready(function () {
+
+
+    var urole = $.getUrlVar('urole');
+    if (urole == 1) {
+        window.onload = inGetUserNowPage($('#userPageNow').html());
+        $('#nextUserPage').click(
+            function () {
+
+                var userpage = $('#userPageNow').html();
+                var totalPage = $('#totalUserPage').html();
+                if (userpage < totalPage) {
+                    $('#userPageNow').html(userpage * 1 + 1 * 1);
+                    inGetUserNowPage(userpage * 1 + 1 * 1);
+                } else {
+                    alert("已到最后一页");
+                }
+
+            }
+        );
+        $('#preUserPage').click(
+            function () {
+
+                var userpage = $('#userPageNow').html();
+                if (userpage > 1) {
+                    $('#userPageNow').html(userpage * 1 - 1 * 1);
+                    inGetUserNowPage(userpage * 1 - 1 * 1);
+                } else {
+                    alert("已到第一页");
+                }
+            }
+        );
+
+
+        $('#firstUserPage').click(
+            function () {
+                inGetUserNowPage(1);
+                $('#userPageNow').html(1);
+            }
+        );
+
+        $('#endUserPage').click(
+            function () {
+                inGetUserNowPage($('#totalUserPage').html());
+                $('#userPageNow').html($('#totalUserPage').html());
+            }
+        );
+
+
+        $('#deleteUser').click(
+            function () {
+                var userIdArray = new Array();
+                var i = 0;
+                $("input:checkbox[name='usercheck']:checked").each(function () {
+                    userIdArray[i++] = parseInt($(this).attr("id"));
+                });
+                if (userIdArray.length == 0) {
+                    alert("请选择要删除的用户");
+                } else {
+                    var userIds = userIdArray.join("/");
+                    $.ajax({
+                        type: 'get',
+                        url: '/oms/user/deleteUserByIds',
+                        data: {
+                            userIdList: userIds
+                        },
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (data) {
+                            if (data > 0) {
+                                inGetUserNowPage(1);
+                                alert("删除成功");
+                            } else {
+                                alert("删除失败");
+                            }
+                        },
+                        error: function (data) {
+                            alert("删除失败");
+                            inGetUserNowPage(1);
+                        }
+
+                    });
+                }
+            });
+
+        $("#addUser").click(function () {
+
+            var username = $('#addUserName').val().trim();
+            var password = $('#addUserPassword').val().trim();
+            if (username == '') {
+                alert("请输入用户名");
+            } else {
+                if (password == '') {
+                    alert("请输入密码");
+                } else {
+                    if (password.length < 6) {
+                        alert("密码不得少于6位数");
+                    } else {
+                        var zzbds = /^([\u4E00-\u9FA5]|\w)*$/;
+                        if (!zzbds.test(username)) {
+                            alert("请输入有效用户名");
+                        } else {
+                            if (!zzbds.test(password)) {
+                                alert("请输入有效密码");
+                            } else {
+                                $.ajax({
+                                    type: 'get',
+                                    url: '/oms/user/addUser',
+                                    data: {
+                                        userName: username,
+                                        password: password
+                                    },
+                                    contentType: "application/json; charset=utf-8",
+                                    dataType: "json",
+                                    success: function (data) {
+                                        if (data == 1) {
+                                            alert("添加成功");
+                                            $('#addUserName').val("");
+                                            $('#addUserPassword').val("");
+                                            inGetUserNowPage($('#totalUserPage').html());
+                                        } else {
+                                            alert("用户名已存在");
+                                            $('#addUserName').val("");
+                                            $('#addUserPassword').val("");
+                                        }
+                                    },
+                                    error: function (data) {
+                                        alert("添加用户失败");
+                                    }
+
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+
+        });
+
+
+        $("#updateUser").click(function () {
+
+            var username = $('#updateUserName').val().trim();
+            var password = $('#updateUserPassword').val().trim();
+            var userIdArray = new Array();
+            var i = 0;
+            $("input:checkbox[name='usercheck']:checked").each(function () {
+                userIdArray[i++] = parseInt($(this).attr("id"));
+            });
+            if (username == '') {
+                alert("请输入用户名");
+            } else {
+                if (password == '') {
+                    alert("请输入密码");
+                } else {
+                    if (password.length < 6) {
+                        alert("密码不得少于6位数");
+                    } else {
+                        var zzbds = /^([\u4E00-\u9FA5]|\w)*$/;
+                        if (!zzbds.test(username)) {
+                            alert("请输入有效用户名");
+                        } else {
+                            if (!zzbds.test(password)) {
+                                alert("请输入有效密码");
+                            } else {
+                                var userIds = userIdArray.join("/");
+                                $.ajax({
+                                    type: 'get',
+                                    url: '/oms/user/updateUser',
+                                    data: {
+                                        uid: userIds,
+                                        userName: username,
+                                        password: password
+                                    },
+                                    contentType: "application/json; charset=utf-8",
+                                    dataType: "json",
+                                    success: function (data) {
+                                        if (data == 1) {
+                                            alert("修改成功");
+                                            $('#updateUserName').val("");
+                                            $('#updateUserPassword').val("");
+                                            inGetUserNowPage(1);
+                                        } else {
+                                            alert("用户名已存在");
+                                            $('#updateUserName').val("");
+                                            $('#updateUserPassword').val("");
+                                        }
+                                    },
+                                    error: function (data) {
+                                        alert("修改用户失败");
+                                    }
+
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        $('#userselectbutton').click(function () {
+
+            var select = $('#userselectvalue').val().trim();
+            if (select.length == 0) {
+
+                alert("请输入查询内容");
+            } else {
+                var zzbds = /^([\u4E00-\u9FA5]|\w)*$/;
+                if (!zzbds.test(select)) {
+                    alert("请不要输入特殊符号");
+                }else {
+                    selectByUserName(1, select);
+                    $('#preUserPage').hide();
+                    $('#nextUserPage').hide();
+                    $('#endUserPage').hide();
+                }
+            }
+
+        });
+
+        function inGetUserNowPage(pageNow) {
+            var page = pageNow;
+            var pageSize = 20;
+            $.ajax({
+                type: 'get',
+                url: '/oms/user/getAllUsers',
+                data: {
+                    nowPage: page,
+                    pageSize: pageSize
+                },
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (data) {
+                    //alert(data.userList[0].uid);
+                    var userList = data.userList;
+                    var totalPage = data.totalPage;
+                    $('#usertbody').html("");
+                    for (var i in userList) {
+                        var id = i * 1 + 1 * 1;
+                        $('#usertbody').append("<tr><td>" + id + "</td><td><input type='checkbox' id='" + userList[i].uid + "user" + "' name='usercheck' onclick='usercheckclick(this.id)'></td><td><a>" + userList[i].uname + "</a></td> <td>&nbsp;" + userList[i].upassword + "</td> <td>&nbsp;" + userList[i].urole + "</td> </tr>");
+                        $('#totalUserPage').html(totalPage);
+                    }
+
+
+                },
+                error: function (data) {
+                    alert("获取用户列表失败");
+                }
+
+            });
+        }
+
+        function selectByUserName(pageNow, userName) {
+            var page = pageNow;
+            var username = userName;
+            var pageSize = 20;
+            $.ajax({
+                type: 'get',
+                url: '/oms/user/selectUserByName',
+                data: {
+                    username: username,
+                    nowPage: page,
+                    pageSize: pageSize
+                },
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (data) {
+                    var userList = data.userList;
+                    var totalPage = data.totalPage;
+                    $('#usertbody').html("");
+                    for (var i in userList) {
+                        var id = i * 1 + 1 * 1;
+                        $('#usertbody').append("<tr><td>" + id + "</td><td><input type='checkbox' id='" + userList[i].uid + "user" + "' name='usercheck' onclick='usercheckclick(this.id)'></td><td><a>" + userList[i].uname + "</a></td> <td>&nbsp;" + userList[i].upassword + "</td> <td>&nbsp;" + userList[i].urole + "</td> </tr>");
+                        $('#totalUserPage').html(totalPage);
+                    }
+                },
+                error: function (data) {
+                    alert("查询失败");
+                }
+
+            });
+        }
+
+    } else {
+
+        alert("普通用户");
+    }
+});
