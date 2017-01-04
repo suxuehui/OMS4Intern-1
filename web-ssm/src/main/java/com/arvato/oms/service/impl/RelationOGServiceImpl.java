1a60ad751c87cdc68e2e05d006f755d50e1615b4
龚云芳 <gyunfljt@outlook.com>
2017-01-04 14:27:04 +0800

    relationOGService方法的修改

--- web-ssm/src/main/java/com/arvato/oms/service/impl/RelationOGServiceImpl.java
+++ web-ssm/src/main/java/com/arvato/oms/service/impl/RelationOGServiceImpl.java
@@ -21,5 +21,8 @@ public class RelationogServiceImpl implements RelationogService {
         return relationogModelMapper.selectMessageByOid(oid);
     }
 
+    public Integer selectGoodsRnum(String oId) {
+        return relationogModelMapper.selectGoodsRnum(oId);
+    }
 }
 
