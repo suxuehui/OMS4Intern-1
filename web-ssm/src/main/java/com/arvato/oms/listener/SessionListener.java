d2760ba12cf6fe44b4ac980f0e6195aa48ef1581
张伟 <1570728932@qq.com>
2017-01-10 14:03:41 +0800

    同一用户不同浏览器登录，注销上一个登录

---
+++ web-ssm/src/main/java/com/arvato/oms/listener/SessionListener.java
@@ -0,0 +1,40 @@
+package com.arvato.oms.listener;
+
+import javax.servlet.http.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ZHAN545 on 2017/1/10.
+ */
+public class SessionListener implements HttpSessionAttributeListener{
+
+    Map<String, HttpSession> map = new HashMap<String, HttpSession>();
+
+    public void attributeAdded(HttpSessionBindingEvent httpSessionBindingEvent) {
+        HttpSession httpSession=httpSessionBindingEvent.getSession();
+        String name=(String)httpSession.getAttribute("uname");
+        if(map.containsKey(name))
+        {
+            HttpSession session=map.get(name);
+            if(!httpSession.getId().equals(session.getId()))
+            {
+                map.remove(name);
+                session.invalidate();
+            }
+        }
+        map.put(name,httpSession);
+    }
+
+    public void attributeRemoved(HttpSessionBindingEvent httpSessionBindingEvent) {
+        String name=httpSessionBindingEvent.getName();
+        if(map.containsKey(name))
+        {
+            map.remove(name);
+        }
+    }
+
+    public void attributeReplaced(HttpSessionBindingEvent httpSessionBindingEvent) {
+
+    }
+}
